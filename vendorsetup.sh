70ba6d8dd3840047e29c27ed113ada0428fc1666
NameLess, the Jedi <yoddah@gmail.com>
2010-09-09 13:12:29 +0200

    Initial import of kernel config (it's Cyanogen config minus NFS) vendorsetup.sh changed so it copies my config to proper place

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,4 @@ add_lunch_combo cyanogen_dream_sapphire-eng
 add_lunch_combo nlj_dream_sapphire-eng
 
 PATH=$PATH:$PWD/vendor/cyanogen/tools ; export PATH
+cp $PWD/vendor/nlj/nlj_msm_defconfig kernel-msm/arch/arm/configs/
