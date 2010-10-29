574a760867afe829c0cda69595bb65b2ecf71e77
ezterry <ezterry3@gmail.com>
2010-10-29 00:11:37 -0400

    set ril values in device settings

--- products/common.mk
+++ products/common.mk
@@ -27,8 +27,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.wifi-watchlist=GoogleGuest \
     ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dateformat=MM-dd-yyyy \
-    ro.ril.hsxpa=2 \
-    ro.ril.gprsclass=12 \
     ro.com.android.dataroaming=false
 
 # CyanogenMod specific product packages
