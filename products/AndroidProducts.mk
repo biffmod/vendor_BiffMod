1aeee22df2ec1a19f04daaef17acca79bdb460de
ezterry <ezterry3@gmail.com>
2010-10-30 19:42:43 -0400

    This logic adds wireless tether for root users to the biffmod build: http://code.google.com/p/android-wifi-tether/
    Since the user may update or other operation in the future we just download
    the binary from their website and place it in the update.zip as is.
    
    * download will happen on running environment setup if the file is not found
    * Install happens as the last step of 'make biffmod-ota'
    
    The licence of the binary/code of android-wifi-tether is GPLv3 thus we ought to
    not have any complaints of including it.
    
    BiffMod users who find the app useful should be encouraged to donate
    to the android-wifi-tether creators (as indicated by the app)

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -4,4 +4,5 @@ PRODUCT_MAKEFILES := \
 biffmod-ota: otapackage
 	echo "SquishIt :)"
 	./vendor/biffmod/tools/squisher
+	./vendor/biffmod/tools/add-wifitether
 
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,13 @@
 add_lunch_combo biffmod_dream_sapphire-eng
 
 PATH=$PATH:$PWD/vendor/biffmod/tools ; export PATH
+
+if [ -f $PWD/vendor/biffmod/wireless_tether_2_0_2.apk ]
+then
+    echo wireless_tether_2_0_2.apk already downloaded
+    echo please continue with the build
+else
+    echo Downloading wireless_tether_2_0_2.apk
+    curl -o $PWD/vendor/biffmod/wireless_tether_2_0_2.apk http://android-wifi-tether.googlecode.com/files/wireless_tether_2_0_2.apk
+    echo wireless_tether_2_0_2.apk downloaded";" you can now run the build
+fi
